1eb0af157ea1ac8349e98f77af1de3c000611dcb
Marlon Valbuena <marlon_l_g@outlook.com>
2024-09-30 17:24:11 -0500

    Conexiones prod

--- Back_Finan/src/main/java/com/Back_Finan/controller/UsuarioController.java
+++ Back_Finan/src/main/java/com/Back_Finan/controller/UsuarioController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:8080")
 @RequestMapping("/api/usuarios")
 public class UsuarioController {
 
