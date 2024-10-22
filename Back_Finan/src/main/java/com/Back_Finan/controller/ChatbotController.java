57badeacc4bf2d800e540edd69557f3ed4804858
Marlon Valbuena <marlon_l_g@outlook.com>
2024-10-22 13:40:36 -0500

    conexiones pro

--- Back_Finan/src/main/java/com/Back_Finan/controller/ChatbotController.java
+++ Back_Finan/src/main/java/com/Back_Finan/controller/ChatbotController.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://192.168.0.4:8082")
 @RequestMapping("/api/chatbot")
 public class ChatbotController {
 
